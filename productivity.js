08a984278d93fafdbace95859b5cde1403b94f79
Krause <krau0031@STTHOMAS.EDU>
2018-02-23 10:38:45 -0600

    new task

--- productivity.js
+++ productivity.js
@@ -1,11 +1,17 @@
 window.onload = function()
 {
 	var add = document.getElementById("add-button");
+	var submitTask = document.getElementById("submit-task");
 	add.onclick = open;
+	submitTask.onclick = newTask;
 }
 
 function open()
 {
 	var par = this.parentElement;
 	par.innerHTML = par.innerHTML;
+}
+
+function newTask()
+{
 }
